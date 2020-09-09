62a97f7b50faeb37dfbf893e1ad5e0f0c50d69dd
Vincent LOPEZ <vincent.lopez.external@zalando.de>
2020-09-09 17:30:40 +0200

    added first version of lefigaro engine

--- source/engines/lefigaro.py
+++ source/engines/lefigaro.py
@@ -1,68 +1,51 @@
-import requests
-from bs4 import BeautifulSoup
 
-BASE_URL = "https://proprietes.lefigaro.fr/"
+class LeFigaroProprietes:
 
-
-class ChateauCrawler:
-
-    def __init__(self):
-        self.base_url = BASE_URL
-
-    def process_page(self, url):
-        page = requests.get(url)
-        return BeautifulSoup(page.content, 'html.parser')
+    MIN_TIME_BEFORE_CALL = 2
+    VERBOSITY = 1
+    BASE_URL = "https://proprietes.lefigaro.fr/"
 
     def get_catalog_page_url(self, nb=1):
         return f"{self.base_url}/annonces/chateau/?page={nb}"
 
-    def get_ad_page_url(self, ad_path):
-        return f"{self.base_url}/{ad_path}"
-
-    def catalog_get_list_ads(self, page_nb=1):
-        soup = self.process_page(self.get_catalog_page_url(page_nb))
-        list = soup.findAll("div", {"class": "container-itemlist-inline js-itemlist-inline"})
-        return [ad.a['href'] for ad in list]
-
-    def reload_ad_list(self):
-        first_page = self.process_page(self.get_catalog_page_url())
+    def list_catalog_target_pages(self, proc):
+        catalog_search_domain =[]
+        first_page = proc(self.get_catalog_page_url())
         nb_pages = int(first_page.find("ul", {"class": "pagination"}).findAll("li")[-2].a["data-page"])
-        complete_ad_list = []
-        for i in range(1, nb_pages+1):
-            print(f"Fetching catalog page {i}")
-            complete_ad_list += self.catalog_get_list_ads(page_nb=i)
-            if i>5:
-                break
-        print(complete_ad_list)
+        catalog_search_domain += [self.get_catalog_page_url(nb=nb) for nb in range(1, nb_pages+1)]
+        print(f"Found {len(catalog_search_domain)} catalog pages to process.")
+        return catalog_search_domain
 
-    def list_ad(self, ad_url):
-        soup = self.process_page(self.get_ad_page_url(ad_url))
+    def extract_adlist_from_catalog_page(self, page_url, page):
+        try:
+            ad_list = page.findAll("div", {"class": "container-itemlist-inline js-itemlist-inline"})
+            return [(page_url, ad.a['href'].replace(self.BASE_URL, "")) for ad in ad_list]
+        except:
+            print(f"ERROR while processing catalog page: {page_url}")
+
+    def process_ad_page(self, ad_url, ad_page):
+
         # link ref
-        link_ref = soup.find("link", {"rel": "canonical"})
+        link_ref = ad_page.find("link", {"rel": "canonical"})
 
         # price
-        price = soup.find("span", {"class": "nb-price"})
+        price = ad_page.find("span", {"class": "nb-price"})
 
         # stats
         # TODO detail
-        for t in lol[0].findAll("span", {"class": "nb"}):
+        for t in ad_page[0].findAll("span", {"class": "nb"}):
             print(t.get_text())
 
         # desc
-        description = soup.findAll("p", {"id": "js-description", "itemprop": "description"})
+        description = ad_page.findAll("p", {"id": "js-description", "itemprop": "description"})
 
         # caracteristiques
-        characteritics = soup.findAll("div", {"class": "caracteristiques"})[0].findAll("p", {"class": "h2-like"})
+        characteritics = ad_page.findAll("div", {"class": "caracteristiques"})[0].findAll("p", {"class": "h2-like"})
 
         #ref annonceur
-        ref_advertiser = soup.findAll("p", {"class": "ref-annonce"})
+        ref_advertiser = ad_page.findAll("p", {"class": "ref-annonce"})
 
         # geoloc
-        location = soup.findAll("span", {"class": "name", "itemprop": "addressLocality"})[0].get_text()
+        location = ad_page.findAll("span", {"class": "name", "itemprop": "addressLocality"})[0].get_text()
 
-        return link_ref, price, description, characteritics, ref_advertiser, location
-
-
-lol = ChateauCrawler()
-
-lol.reload_ad_list()
+        return [ad_url, link_ref, price, description, characteritics, ref_advertiser, location]
